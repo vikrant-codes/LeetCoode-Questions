71e5b6ef3b315054b5e378616c92c78bf548371f
vikrant-codes <vikrantbele0673@gmail.com>
2022-10-29 14:57:56 +0530

    commit by vikrant

---
+++ linkedList.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+typedef struct Node
+{
+	int data ;
+	struct Node *next ;
+}	Node ;
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8 } ;
+	int size = sizeof( arr ) / sizeof( arr[0] ) ;
+	printf("***\n");
+}
+
